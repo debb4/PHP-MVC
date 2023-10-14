d2f28b4d02a954659abbae81300a13a3a7f8e540
hakimlutfi46 <lutfihakim1606@gmail.com>
2023-10-14 11:12:21 +0700

    add view code register

---
+++ app/views/login/register.php
@@ -0,0 +1,20 @@
+<div class="container">
+    <h4 class="text-center">Form Register</h4>
+    <hr>
+    <form action="http://localhost/phpmvc/public/login/cekLogin" method="post">
+        <div class="form-group">
+            <label>Username</label>
+            <input type="text" name="username" class="form-control" placeholder="Masukkan Username Anda" value="">
+        </div>
+        <div class="form-group">
+            <label>Password</label>
+            <input type="password" name="password" class="form-control" placeholder="Masukkan Password Anda">
+        </div>
+        <div class="form-group">
+            <label>Full Name</label>
+            <input type="text" name="full_name" class="form-control" placeholder="Masukkan Nama Anda">
+        </div>
+        <button type="submit" name="submit" class="btn btn-primary">SUBMIT</button>
+        <a href="http://localhost/phpmvc/public/register/" class="btn btn-success">Register</a>
+    </form>
+</div>
